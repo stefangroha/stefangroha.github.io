3e17aeeb4f665f7c2381331ecd57112ef93de957
Stefan Groha <s.groha@gmail.com>
2025-08-17 13:12:42 -0400

    simplify

--- js/scripts.js
+++ js/scripts.js
@@ -3,50 +3,92 @@
     * Copyright 2013-2020 Start Bootstrap
     * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
     */
-    (function ($) {
-    "use strict"; // Start of use strict
 
-    // Smooth scrolling using jQuery easing
-    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
-        if (
-            location.pathname.replace(/^\//, "") ==
-                this.pathname.replace(/^\//, "") &&
-            location.hostname == this.hostname
-        ) {
-            var target = $(this.hash);
-            target = target.length
-                ? target
-                : $("[name=" + this.hash.slice(1) + "]");
-            if (target.length) {
-                $("html, body").animate(
-                    {
-                        scrollTop: target.offset().top,
-                    },
-                    1000,
-                    "easeInOutExpo"
-                );
-                return false;
+// Smooth scrolling using native JavaScript
+document.querySelectorAll('a.js-scroll-trigger[href*="#"]:not([href="#"])').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.hash;
+        const targetElement = document.querySelector(targetId);
+
+        if (targetElement) {
+            targetElement.scrollIntoView({
+                behavior: 'smooth'
+            });
+
+            // Close responsive menu after click
+            const navbarCollapse = document.querySelector('.navbar-collapse');
+            if (navbarCollapse && navbarCollapse.classList.contains('show')) {
+                const bsCollapse = new bootstrap.Collapse(navbarCollapse, { toggle: false });
+                bsCollapse.hide();
             }
         }
     });
-
-    // Closes responsive menu when a scroll trigger link is clicked
-    $(".js-scroll-trigger").click(function () {
-        $(".navbar-collapse").collapse("hide");
-    });
-
-    // Activate scrollspy to add active class to navbar items on scroll
-    $("body").scrollspy({
-        target: "#sideNav",
-    });
-})(jQuery); // End of use strict
+});
 
 // Function to copy bibtex to clipboard
 function copyToClipboard(elementId) {
-    var textToCopy = document.querySelector(elementId).textContent;
+    const textToCopy = document.querySelector(elementId).textContent;
     navigator.clipboard.writeText(textToCopy).then(function() {
         console.log('Bibtex copied to clipboard');
     }, function(err) {
         console.error('Could not copy text: ', err);
     });
 }
+
+// Activate scrollspy to add active class to navbar items on scroll
+const scrollSpy = new bootstrap.ScrollSpy(document.body, {
+    target: '#mainNav',
+    offset: 74
+});
+
+// Collapse Navbar
+const navbarCollapse = function () {
+    const navbar = document.body.querySelector('#mainNav');
+    if (!navbar) {
+        return;
+    }
+    if (window.scrollY === 0) {
+        navbar.classList.remove('navbar-shrink')
+    } else {
+        navbar.classList.add('navbar-shrink')
+    }
+
+};
+
+// Collapse now if page is not at top
+navbarCollapse();
+// Collapse the navbar when page is scrolled
+document.addEventListener('scroll', navbarCollapse);
+
+// Google Analytics event tracking for publication links
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('.publication-link').forEach(link => {
+        link.addEventListener('click', function() {
+            gtag('event', 'publication_click', {
+                'event_category': 'Publications',
+                'event_label': this.href
+            });
+        });
+    });
+});
+
+
+
+// Back to top button functionality (if you decide to add one)
+// window.addEventListener('scroll', function() {
+//     const backToTopButton = document.getElementById('back-to-top');
+//     if (backToTopButton) {
+//         if (window.pageYOffset > 100) { // Show button after scrolling down 100px
+//             backToTopButton.classList.add('show');
+//         } else {
+//             backToTopButton.classList.remove('show');
+//         }d
+//     }
+// });
+
+// document.getElementById('back-to-top').addEventListener('click', function(e) {
+//     e.preventDefault();
+//     window.scrollTo({ top: 0, behavior: 'smooth' });
+// });
