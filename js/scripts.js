0e3633c68a516298995d9a19d3320e16254f9579
Stefan Groha <s.groha@gmail.com>
2025-08-17 13:50:51 -0400

    add google analytics

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// Smooth scrolling for navigation links
 document
   .querySelectorAll('a.js-scroll-trigger[href*="#"]:not([href="#"])')
   .forEach((anchor) => {
@@ -8,16 +9,19 @@ document
       const targetElement = document.querySelector(targetId);
 
       if (targetElement) {
+        // Adjust header offset for smooth scrolling
         const headerOffset = 50;
         const elementPosition = targetElement.getBoundingClientRect().top;
         const offsetPosition =
           elementPosition + window.pageYOffset - headerOffset;
 
+        // Scroll to the target element with smooth behavior
         window.scrollTo({
           top: offsetPosition,
           behavior: "smooth",
         });
 
+        // Collapse responsive navbar after clicking a link
         const navbarCollapse = document.querySelector(".navbar-collapse");
         if (navbarCollapse && navbarCollapse.classList.contains("show")) {
           const bsCollapse = new bootstrap.Collapse(navbarCollapse, {
@@ -29,6 +33,7 @@ document
     });
   });
 
+// Function to copy bibtex content to clipboard
 (function () {
   window.copyToClipboard = function (elementId) {
     const textToCopy = document.querySelector(elementId).textContent;
@@ -43,17 +48,20 @@ document
   };
 })();
 
+// Activate scrollspy for navigation highlighting
 const scrollSpy = new bootstrap.ScrollSpy(document.body, {
   target: "#mainNav",
   offset: 74,
 });
 
+// Collapse navbar on scroll
 (function () {
   const navbarCollapse = function () {
     const navbar = document.body.querySelector("#mainNav");
     if (!navbar) {
       return;
     }
+    // Add or remove 'navbar-shrink' class based on scroll position
     if (window.scrollY === 0) {
       navbar.classList.remove("navbar-shrink");
     } else {
@@ -61,10 +69,13 @@ const scrollSpy = new bootstrap.ScrollSpy(document.body, {
     }
   };
 
+  // Collapse the navbar immediately if not at the top of the page
   navbarCollapse();
+  // Add event listener for collapsing navbar on scroll
   document.addEventListener("scroll", navbarCollapse);
 })();
 
+// Google Analytics event tracking for publication links
 document.addEventListener("DOMContentLoaded", function () {
   document.querySelectorAll(".publication-link").forEach((link) => {
     link.addEventListener("click", function () {
@@ -77,4 +88,3 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
-
