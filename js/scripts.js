8e22ec2d5d0407c9812f8e9dd9d1de1bb48a263f
Stefan Groha <s.groha@gmail.com>
2025-08-17 13:46:28 -0400

    slight UI changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,94 +1,94 @@
 /*!
-    * Start Bootstrap - Resume v6.0.1 (https://startbootstrap.com/template-overviews/resume)
-    * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
-    */
+ * Start Bootstrap - Resume v6.0.1 (https://startbootstrap.com/template-overviews/resume)
+ * Copyright 2013-2020 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+ */
 
 // Smooth scrolling using native JavaScript
-document.querySelectorAll('a.js-scroll-trigger[href*="#"]:not([href="#"])').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
+document
+  .querySelectorAll('a.js-scroll-trigger[href*="#"]:not([href="#"])')
+  .forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
 
-        const targetId = this.hash;
-        const targetElement = document.querySelector(targetId);
+      const targetId = this.hash;
+      const targetElement = document.querySelector(targetId);
 
-        if (targetElement) {
-            targetElement.scrollIntoView({
-                behavior: 'smooth'
-            });
+      if (targetElement) {
+        const headerOffset = 50; // Adjust this value as needed
+        const elementPosition = targetElement.getBoundingClientRect().top;
+        const offsetPosition =
+          elementPosition + window.pageYOffset - headerOffset;
 
-            // Close responsive menu after click
-            const navbarCollapse = document.querySelector('.navbar-collapse');
-            if (navbarCollapse && navbarCollapse.classList.contains('show')) {
-                const bsCollapse = new bootstrap.Collapse(navbarCollapse, { toggle: false });
-                bsCollapse.hide();
-            }
+        window.scrollTo({
+          top: offsetPosition,
+          behavior: "smooth",
+        });
+
+        // Close responsive menu after click
+        const navbarCollapse = document.querySelector(".navbar-collapse");
+        if (navbarCollapse && navbarCollapse.classList.contains("show")) {
+          const bsCollapse = new bootstrap.Collapse(navbarCollapse, {
+            toggle: false,
+          });
+          bsCollapse.hide();
         }
+      }
     });
-});
+  });
 
 // Function to copy bibtex to clipboard
-function copyToClipboard(elementId) {
+(function () {
+  window.copyToClipboard = function (elementId) {
     const textToCopy = document.querySelector(elementId).textContent;
-    navigator.clipboard.writeText(textToCopy).then(function() {
-        console.log('Bibtex copied to clipboard');
-    }, function(err) {
-        console.error('Could not copy text: ', err);
-    });
-}
+    navigator.clipboard.writeText(textToCopy).then(
+      function () {
+        console.log("Bibtex copied to clipboard");
+      },
+      function (err) {
+        console.error("Could not copy text: ", err);
+      },
+    );
+  };
+})();
 
 // Activate scrollspy to add active class to navbar items on scroll
 const scrollSpy = new bootstrap.ScrollSpy(document.body, {
-    target: '#mainNav',
-    offset: 74
+  target: "#mainNav",
+  offset: 74,
 });
 
 // Collapse Navbar
-const navbarCollapse = function () {
-    const navbar = document.body.querySelector('#mainNav');
+(function () {
+  const navbarCollapse = function () {
+    const navbar = document.body.querySelector("#mainNav");
     if (!navbar) {
-        return;
+      return;
     }
     if (window.scrollY === 0) {
-        navbar.classList.remove('navbar-shrink')
+      navbar.classList.remove("navbar-shrink");
     } else {
-        navbar.classList.add('navbar-shrink')
+      navbar.classList.add("navbar-shrink");
     }
+  };
 
-};
-
-// Collapse now if page is not at top
-navbarCollapse();
-// Collapse the navbar when page is scrolled
-document.addEventListener('scroll', navbarCollapse);
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  document.addEventListener("scroll", navbarCollapse);
+})();
 
 // Google Analytics event tracking for publication links
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelectorAll('.publication-link').forEach(link => {
-        link.addEventListener('click', function() {
-            gtag('event', 'publication_click', {
-                'event_category': 'Publications',
-                'event_label': this.href
-            });
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll(".publication-link").forEach((link) => {
+    link.addEventListener("click", function () {
+      if (typeof gtag === "function") {
+        gtag("event", "publication_click", {
+          event_category: "Publications",
+          event_label: this.href,
         });
+      }
     });
+  });
 });
 
-
-
-// Back to top button functionality (if you decide to add one)
-// window.addEventListener('scroll', function() {
-//     const backToTopButton = document.getElementById('back-to-top');
-//     if (backToTopButton) {
-//         if (window.pageYOffset > 100) { // Show button after scrolling down 100px
-//             backToTopButton.classList.add('show');
-//         } else {
-//             backToTopButton.classList.remove('show');
-//         }d
-//     }
-// });
-
-// document.getElementById('back-to-top').addEventListener('click', function(e) {
-//     e.preventDefault();
-//     window.scrollTo({ top: 0, behavior: 'smooth' });
-// });
